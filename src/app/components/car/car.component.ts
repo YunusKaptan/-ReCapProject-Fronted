e3e4859d9e0904558e9c23971f8db1ea4cc9bace
Yunus <83038643+YunusKaptan@users.noreply.github.com>
2022-06-13 14:37:55 +0200

    generated filter text for cars

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -24,6 +24,7 @@ export class CarComponent implements OnInit {
   imageOfPath:string;  
   brands:Brand[] = [];
   colors:Color[] = [];
+  filterText="";
 
   constructor(
   private carService: CarService,
